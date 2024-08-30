b58b2b138aab249a7f938186226d7f3ef2eaf27c
Lucas <llovizzio@gmail.com>
2024-08-30 15:57:39 -0300

    rm CuentaBancariaImpl.java

--- tp1ej2/src/ar/unnoba/edu/ipoo/CuentaBancariaImpl.java
+++
@@ -1,7 +0,0 @@
-package ar.unnoba.edu.ipoo;
-
-public class CuentaBancariaImpl extends CuentaBancaria {
-    public CuentaBancariaImpl(Persona titular, double saldoInicial) {
-        super(titular, saldoInicial);
-    }
-}
