de6576131bc1e12f542547101db13931e267ba2c
Lucas <llovizzio@gmail.com>
2024-09-28 14:22:59 -0300

    minor changes

--- tp1ej5/src/Main.java
+++ tp1ej5/src/Main.java
@@ -1,8 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-package ar.unnoba.edu.ipoo;
 
 
+import ar.unnoba.edu.ipoo.ClienteCorreo;
+import ar.unnoba.edu.ipoo.Contacto;
+import ar.unnoba.edu.ipoo.Correo;
+import ar.unnoba.edu.ipoo.Cuenta;
 
 public class Main {
     public static void main(String[] args) {
