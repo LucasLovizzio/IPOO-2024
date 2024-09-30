d00da6d1f6da18fc93d8b5d213ab845f909b89f6
Lucas <llovizzio@gmail.com>
2024-09-30 17:58:16 -0300

    +tp3ej12

---
+++ tp3ej12/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        List<String> myList = Arrays.asList("a1", "b1", "c2", "c1");
+        myList.stream()
+                .filter(s -> s.startsWith("c"))
+                .map(String::toUpperCase)
+                .sorted()
+                .forEach(System.out::println);
+    }
+}
