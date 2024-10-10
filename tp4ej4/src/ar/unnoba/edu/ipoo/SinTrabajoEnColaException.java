247fa09ce99fb525a25e069fa7a106f48046f3ed
Lucas <llovizzio@gmail.com>
2024-10-10 14:52:37 -0300

    + getter, setter

--- tp4ej4/src/ar/unnoba/edu/ipoo/SinTrabajoEnColaException.java
+++ tp4ej4/src/ar/unnoba/edu/ipoo/SinTrabajoEnColaException.java
@@ -1,3 +1,5 @@
+package ar.unnoba.edu.ipoo;
+
 public class SinTrabajoEnColaException extends Exception {
 
     private String nombre;
@@ -7,8 +9,16 @@ public class SinTrabajoEnColaException extends Exception {
         this.nombre = nombre;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     @Override
     public String getMessage() {
-        return super.getMessage() + "\n Nombre de la cola: " + nombre;
+        return super.getMessage() + "\n Nombre de la cola: " + getNombre();
     }
 }
