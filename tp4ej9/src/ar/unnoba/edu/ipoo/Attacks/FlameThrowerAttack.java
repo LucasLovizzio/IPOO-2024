e4af85f5b6ca4965024ee9c83a37ceef892559f5
Lucas <llovizzio@gmail.com>
2024-10-13 14:40:47 -0300

    +Attack Strategies added

---
+++ tp4ej9/src/ar/unnoba/edu/ipoo/Attacks/FlameThrowerAttack.java
@@ -0,0 +1,10 @@
+package ar.unnoba.edu.ipoo.Attacks;
+
+import ar.unnoba.edu.ipoo.AttackStrategy;
+
+public class FlameThrowerAttack implements AttackStrategy {
+    @Override
+    public int attack() {
+        return 100;
+    }
+}
---
+++ tp4ej9/src/ar/unnoba/edu/ipoo/Attacks/LaserAttack.java
@@ -0,0 +1,10 @@
+package ar.unnoba.edu.ipoo.Attacks;
+
+import ar.unnoba.edu.ipoo.AttackStrategy;
+
+public class LaserAttack implements AttackStrategy {
+    @Override
+    public int attack() {
+        return 90;
+    }
+}
